acb755919732b7f46a1efe4d347fc0addce63ba9
David <rahrahfez@hotmail.com>
2020-09-08 23:43:40 -0400

    removed merge conflict

--- lib/simple_http.ex
+++ lib/simple_http.ex
@@ -1,6 +1,6 @@
 defmodule SimpleHttp do
   require Logger
-<<<<<<< HEAD
+
   @moduledoc """
   Documentation for `SimpleHttp`.
   update dev
@@ -34,47 +34,6 @@ defmodule SimpleHttp do
       id: SimpleHttp,
       start: {SimpleHttp, :start_link, [opts]}
     }
-=======
 
-  def start_link(port: port) do
-    {:ok, socket} =
-      :gen_tcp.listen(port, [active: false, packet: :http_bin, reuseaddr: true])
-    Logger.info("Accepting connections on port #{port}")
-
-    {:ok, spawn_link(SimpleHttp, :accept, [socket])}
-  end
-
-  def accept(socket) do
-    {:ok, request} = :gen_tcp.accept(socket)
-
-    spawn(fn ->
-      body = "Hello world! The time is #{Time.to_string(Time.utc_now())}"
-
-      response = """
-      HTTP/1.1 200\r
-      Content-Type: text/html\r
-      Content-Length: #{byte_size(body)}\r
-      \r
-      #{body}
-      """
-
-      send_response(request, response)
-    end)
-
-    accept(socket)
-  end
-
-  def send_response(socket, response) do
-    :gen_tcp.send(socket, response)
-    :gen_tcp.close(socket)
-  end
-
-  def child_spec(opts) do
-    %{id: SimpleHttp, start: {SimpleHttp, :start_link, [opts]}}
-  end
-
-  def hello do
-    :world
->>>>>>> 777d0ce1c0c7ab9db158d470f224875bc8f1844a
   end
 end
--- lib/simple_http/application.ex
+++ lib/simple_http/application.ex
@@ -2,7 +2,6 @@ defmodule SimpleHttp.Application do
   @moduledoc false
   use Application
 
-<<<<<<< HEAD
   @impl true
   def start(_type, _args) do
     port = String.to_integer(System.get_env("PORT") || "4040")
@@ -15,18 +14,6 @@ defmodule SimpleHttp.Application do
         name: SimpleHttp.Supervisor,
         strategy: :one_for_one
       ]
-=======
-  def start(_type, _args) do
-    port = String.to_integer(System.get_env("PORT") || "4040")
-    IO.puts(port)
-    children = [
-      {SimpleHttp, port}
-    ]
-    opts = [
-      strategy: :one_for_one,
-      name: SimpleHttp.Supervisor
-    ]
->>>>>>> 777d0ce1c0c7ab9db158d470f224875bc8f1844a
 
     Supervisor.start_link(children, opts)
   end

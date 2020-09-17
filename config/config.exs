3ac5cfa5f7ba0cc8d77168f8263987d7151facab
David <rahrahfez@hotmail.com>
2020-09-17 12:46:39 -0400

    added test and dev config files for mock database

--- config/config.exs
+++ config/config.exs
@@ -1,9 +1,11 @@
 import Config
 
+config :simple_http, ecto_repos: [SimpleHttp.Repo]
+
 config :simple_http, SimpleHttp.Repo,
   database: "simple_http_repo",
   username: "postgres",
   password: "pass",
   hostname: "localhost"
 
-  config :simple_http, ecto_repos: [SimpleHttp.Repo]
+  import_config "#{Mix.env()}.exs"
---
+++ config/test.exs
@@ -0,0 +1,3 @@
+import Config
+
+config :simple_http, repo: SimpleHttp.TestRepo
